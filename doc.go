c93d604e69a6e86fc673a941f1a936962dfa29b7
Cameron Newman <cameron@newman.io>
2018-07-26 15:48:47 +1000

    Fixed typo

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
 /*
-Package function is an Golang email validation. You can valid email address formats as well as the domain/MX records
+Package email is an Golang email validation. You can valid email address formats as well as the domain/MX records
 */
 package email
