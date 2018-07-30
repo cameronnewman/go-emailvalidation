276f8420de5661d726a9288b7534fe92bd2ec10e
Cameron Newman <cameron@newman.io>
2018-07-30 20:21:27 +1000

    Updated the godocs

--- doc.go
+++ doc.go
@@ -2,3 +2,14 @@
 Package email is an Golang email validation. You can valid email address formats as well as the domain/MX records
 */
 package email
+
+// Simple example:
+//
+// 	fmt.Println("Hello")
+//	emailaddress := "john.snow@gmaiiiiiiillllll.com"
+//
+//	err := email.New().ValidateEmailAddress(emailaddress)
+//	if err != nil {
+//		fmt.Println(err)
+//	}
+//
