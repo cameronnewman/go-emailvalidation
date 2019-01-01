6bb4d2092b684bb6dccdca21bd0eed6161aa17ce
Travis CI <builds@travis-ci.com>
2019-01-01 20:41:12 +1100

    Updated tagging script

--- build/ci/tag.sh
+++ build/ci/tag.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  // do the deploy
 
-  git config --global user.email "builds@travis-ci.com"
-  git config --global user.name "Travis CI"
-  echo `cat VERSION.txt`
-  export GIT_TAG=`cat VERSION.txt`
-  git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
-  echo `git show origin`
-  git push origin $GIT_TAG
+if [ $TRAVIS ]; then
 
+    if [ $TRAVIS_BRANCH == "master" ]; then
+
+        echo $TRAVIS_BRANCH
+
+        git config --global user.email "builds@travis-ci.com"
+        git config --global user.name "Travis CI"
+        echo `cat VERSION.txt`
+        export GIT_TAG=`cat VERSION.txt`
+        git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
+        echo `git show origin`
+        git push origin $GIT_TAG
+
+    fi
+
+    echo "Build is not in the master branch"
+    exit 1
 fi
+echo "Script hasn't been run on TravisCI platform"
+exit 1
