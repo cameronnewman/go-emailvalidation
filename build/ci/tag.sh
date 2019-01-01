a90595e660469978eb48f9aadfbf2342ca7dc01d
Travis CI <builds@travis-ci.com>
2019-01-01 21:01:55 +1100

    Added PR check

--- build/ci/tag.sh
+++ build/ci/tag.sh
@@ -2,22 +2,27 @@
 
 if [ $TRAVIS ]; then
 
-    if [ $TRAVIS_BRANCH == "master" ]; then
+    if [ !$TRAVIS_PULL_REQUEST]; then
 
-        echo $TRAVIS_BRANCH
+        if [ $TRAVIS_BRANCH == "master" ]; then
 
-        git config --global user.email "builds@travis-ci.com"
-        git config --global user.name "Travis CI"
-        echo `cat VERSION.txt`
-        export GIT_TAG=`cat VERSION.txt`
-        git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
-        echo `git show origin`
-        git push origin $GIT_TAG
+            echo $TRAVIS_BRANCH
 
+            git config --global user.email "builds@travis-ci.com"
+            git config --global user.name "Travis CI"
+            echo `cat VERSION.txt`
+            export GIT_TAG=`cat VERSION.txt`
+            git tag $GIT_TAG -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
+            echo `git show origin`
+            git push origin $GIT_TAG
+
+        fi
+
+        echo "Build is not in the master branch"
+        exit 1
     fi
 
-    echo "Build is not in the master branch"
-    exit 1
+    echo "Not run in a PR"
 fi
 echo "Script hasn't been run on TravisCI platform"
 exit 1
