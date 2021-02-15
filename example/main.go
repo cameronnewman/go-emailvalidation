4521da482310a91650d56e6ac0f298e1106a33f6
Cameron Newman <cameron@newman.io>
2021-02-15 11:09:55 +1100

    Fix example folder and mod file

--- example/main.go
+++ example/main.go
@@ -3,33 +3,33 @@ package main
 import (
 	"fmt"
 
-	email "github.com/cameronnewman/go-emailvalidation/v2"
+	email "github.com/cameronnewman/go-emailvalidation/v3"
 )
 
 func main() {
 
-	emailaddress := "John.Snow@gmaiiiiiiillllll.com"
+	emailAddress := "John.Snow@gmaiiiiiiillllll.com"
 
 	// Run all checks, including validating the format along with DNS lookups which
 	// may be slower depending on your DNS server performance
-	err := email.Validate(emailaddress)
+	err := email.Validate(emailAddress)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	// Checks the format - this function performs no network operations and is very fast
-	err = email.ValidateFormat(emailaddress)
+	err = email.ValidateFormat(emailAddress)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	// Checks domain NS & MX records exist
-	err = email.ValidateDomainRecords(emailaddress)
+	err = email.ValidateDomainRecords(emailAddress)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	// Normalize email address for storage
-	emailNormal := email.Normalize(emailaddress)
+	emailNormal := email.Normalize(emailAddress)
 	fmt.Println(emailNormal)
 }
